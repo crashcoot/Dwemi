faf7284a2e234b9e6d3e8281b8dceb5f787739bc
Crashcoot <derek_s_burney@hotmail.com>
2018-10-28 21:03:38 -0400

    Now uses express server

--- server.js
+++ server.js
@@ -1,81 +1,21 @@
 
 
-const app = require('http').createServer(handler)
-const io = require('socket.io')(app) //wrap server app in socket io capability
-const fs = require("fs") //need to read static files
-const url = require("url") //to parse url strings
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 8080
-app.listen(PORT) //start server listening on PORT
 
-function handler(request, response) {
-  let urlObj = url.parse(request.url, true, false)
-  console.log("\n============================")
-  console.log("PATHNAME: " + urlObj.pathname)
-  console.log("REQUEST: " + ROOT_DIR + urlObj.pathname)
-  console.log("METHOD: " + request.method)
+app.use(express.static('html'))
 
-  let receivedData = ""
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/');
+});
 
-  //attached event handlers to collect the message data
-  request.on("data", function(chunk) {
-    receivedData += chunk
-  })
-
-  //event handler for the end of the message
-  request.on("end", function() {
-    console.log("REQUEST END: ")
-    console.log("received data: ", receivedData)
-    console.log("type: ", typeof receivedData)
-
-   
-
-    if (request.method == "GET") {
-      //handle GET requests as static file requests
-      fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data) {
-        if (err) {
-          //report error to console
-          console.log("ERROR: " + JSON.stringify(err))
-          //respond with not found 404 to client
-          response.writeHead(404)
-          response.end(JSON.stringify(err))
-          return
-        }
-        response.writeHead(200, {
-          "Content-Type": get_mime(urlObj.pathname)
-        })
-        response.end(data)
-      })
-    }
-  })
-}
-
-
-const ROOT_DIR = "html" //dir to serve static files from
-
-const MIME_TYPES = {
-  css: "text/css",
-  gif: "image/gif",
-  htm: "text/html",
-  html: "text/html",
-  ico: "image/x-icon",
-  jpeg: "image/jpeg",
-  jpg: "image/jpeg",
-  js: "application/javascript",
-  json: "application/json",
-  png: "image/png",
-  svg: "image/svg+xml",
-  txt: "text/plain"
-}
-
-function get_mime(filename) {
-  for (let ext in MIME_TYPES) {
-    if (filename.indexOf(ext, filename.length - ext.length) !== -1) {
-      return MIME_TYPES[ext]
-    }
-  }
-  return MIME_TYPES["txt"]
-}
+http.listen(8080, function(){
+  
+});
 
 var sockets = { };
 let interval;
@@ -202,8 +142,7 @@ function upJoy() {
 
 
 
-console.log("Server Running at PORT: 8080  CNTL-C to quit")
-console.log("To Test:")
+console.log("Server Running at PORT: " + PORT + "  CNTL-C to quit");
 console.log("Open several browsers at: http://localhost:8080/dwemi.html")
 
 function pickDirection() {
