69d85bc5849747f3f7d8ec99a1bba8b4455945a6
Crashcoot <derek_s_burney@hotmail.com>
2018-10-31 13:16:14 -0400

    Bigger Dwemi

--- dwemiimage.py
+++ dwemiimage.py
@@ -11,7 +11,7 @@ im1 = im1[:, :, :3]
 
 # gets image2
 im2 = Image.open("html/images/stomachimages/test.jpg")
-im2 = ImageOps.fit(im2, (200,200)) # resizes and crops to 200X200
+im2 = ImageOps.fit(im2, (400,400)) # resizes and crops to 200X200
 im2 = np.asarray(im2)
 im2 = im2[:, :, :3]
 
@@ -19,7 +19,7 @@ im2 = im2[:, :, :3]
 im2Height, im2Width, _ = im2.shape
 
 # mask for choosing chunks of each image
-mask = np.random.binomial(1, 0.98, size=(40,40))    # creates 40x40 array of 1 or 0, 1 for im1 0 for im2
+mask = np.random.binomial(1, 0.80, size=(80,80))    # creates 40x40 array of 1 or 0, 1 for im1 0 for im2
 mask = mask.repeat(5, axis=0).repeat(5, axis=1)     # repeats the mask to be 200x200
 mask = np.expand_dims(mask, axis=-1)                # adds 3rd dimmension to be broadcastable with imgs
 
